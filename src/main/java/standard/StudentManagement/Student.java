5413a1596aea2fd4130eda63b0aacf52bf817217
Ryoma Inagaki <ryoma_bingo0509@ezweb.ne.jp>
2025-03-02 23:43:00 +0900

    application.propertiesの修正 lombok追加 受講生情報のの全件検索 コースの全件検索

--- src/main/java/standard/StudentManagement/Student.java
+++ src/main/java/standard/StudentManagement/Student.java
@@ -1,33 +1,18 @@
 package standard.StudentManagement;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Student {
 
   private String id;
   private String name;
+  private String kanaName;
+  private String nickname;
+  private String email;
+  private String area;
   private int age;
-
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public int getAge() {
-    return age;
-  }
-
-  public void setAge(int age) {
-    this.age = age;
-  }
+  private String sex;
 }
