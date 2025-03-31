3c7908eb0e0c888a1a4443c2e24771d7a85d4f32
Ryoma Inagaki <ryoma_bingo0509@ezweb.ne.jp>
2025-03-31 22:49:20 +0900

    スタンダード第15回課題 コース情報の登録

--- src/main/java/standard/StudentManagement/repository/StudentRepository.java
+++ src/main/java/standard/StudentManagement/repository/StudentRepository.java
@@ -3,6 +3,7 @@ package standard.StudentManagement.repository;
 import java.util.List;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 import standard.StudentManagement.data.Student;
 import standard.StudentManagement.data.StudentCourse;
@@ -22,5 +23,12 @@ public interface StudentRepository {
       VALUES (#{id}, #{name}, #{kanaName}, #{nickname}, #{email}, #{area}, #{age}, #{sex}, #{remark}, false)
       """)
   void registerStudent(Student student);
+
+  @Insert("""
+      INSERT INTO students_courses (student_id, course_name, start_at, end_at)
+      VALUES (#{studentId}, #{courseName}, #{startAt}, #{endAt})
+      """)
+  @Options(useGeneratedKeys = true, keyProperty = "id")
+  void registerStudentCourses(StudentCourse studentCourse);
 
 }
