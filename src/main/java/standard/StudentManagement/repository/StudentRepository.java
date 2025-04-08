66b80dd97ac7e2c72c7c20399fe8a14184875f09
Ryoma Inagaki <ryoma_bingo0509@ezweb.ne.jp>
2025-04-08 21:53:36 +0900

    スタンダード第16回課題 受講生更新画面作成 受講生更新の実装

--- src/main/java/standard/StudentManagement/repository/StudentRepository.java
+++ src/main/java/standard/StudentManagement/repository/StudentRepository.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import standard.StudentManagement.data.Student;
 import standard.StudentManagement.data.StudentCourse;
 
@@ -15,8 +16,15 @@ public interface StudentRepository {
   @Select("SELECT * FROM students")
   List<Student> searchStudent();
 
+  @Select("SELECT * FROM students WHERE id = #{id}")
+  Student searchStudentId(String id);
+
   @Select("SELECT * FROM students_courses")
   List<StudentCourse> searchStudentCourses();
+
+  @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
+  List<StudentCourse> searchStudentCoursesByStudentId(String studentId);
+
 
   @Insert("""
       INSERT INTO students (id, name, kana_name, nickname, email, area, age, sex, remark, is_deleted)
@@ -30,5 +38,18 @@ public interface StudentRepository {
       """)
   @Options(useGeneratedKeys = true, keyProperty = "id")
   void registerStudentCourses(StudentCourse studentCourse);
+
+  @Update("""
+      UPDATE students SET name = #{name}, kana_name = #{kanaName}, nickname = #{nickname},
+      email = #{email}, area = #{area}, age = #{age}, sex = #{sex}, remark = #{remark}, is_deleted = #{isDeleted}
+      WHERE id = #{id}
+      """)
+  void updateStudent(Student student);
+
+  @Update("""
+       UPDATE students_courses SET course_name = #{courseName}
+      WHERE student_id = #{studentId}
+      """)
+  void updateStudentCourses(StudentCourse studentCourse);
 
 }
