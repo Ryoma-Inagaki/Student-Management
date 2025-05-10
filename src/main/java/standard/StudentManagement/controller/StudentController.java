f3ff3484273a3f84ae7bad3ddb21483799c2aeea
Ryoma Inagaki <ryoma_bingo0509@ezweb.ne.jp>
2025-05-10 15:26:14 +0900

    スタンダード第20回課題 受講生一覧取得、受講生登録、更新のAPIをREST形式に変更 htmlの削除

--- src/main/java/standard/StudentManagement/controller/StudentController.java
+++ src/main/java/standard/StudentManagement/controller/StudentController.java
@@ -2,20 +2,19 @@ package standard.StudentManagement.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import standard.StudentManagement.controller.converter.StudentConverter;
 import standard.StudentManagement.data.Student;
 import standard.StudentManagement.data.StudentCourse;
 import standard.StudentManagement.domain.StudentDetail;
 import standard.StudentManagement.service.StudentService;
 
-@Controller
+@RestController
 public class StudentController {
 
   private StudentService service;
@@ -28,12 +27,10 @@ public class StudentController {
   }
 
   @GetMapping("/studentList")
-  public String getStudentList(Model model) {
+  public List<StudentDetail> getStudentList() {
     List<Student> students = service.getStudentList();
     List<StudentCourse> studentCourses = service.getStudentCourseList();
-
-    model.addAttribute("studentList", converter.convertStudentDetails(students, studentCourses));
-    return "studentList";
+    return converter.convertStudentDetails(students, studentCourses);
   }
 
 
@@ -46,27 +43,14 @@ public class StudentController {
   }
 
   @PostMapping("/registerStudent")
-  public String registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
-    if (result.hasErrors()) {
-      return "registerStudent";
-    }
+  public ResponseEntity<String> registerStudent(@RequestBody StudentDetail studentDetail) {
     service.registerStudent(studentDetail);
-    return "redirect:/studentList";
-  }
-
-  @GetMapping("/updateStudent/{id}")
-  public String editStudent(@PathVariable String id, Model model) {
-    StudentDetail studentDetail = service.getStudentProfile(id);
-    model.addAttribute("studentDetail", studentDetail);
-    return "updateStudent";
+    return ResponseEntity.ok("登録が成功しました。");
   }
 
   @PostMapping("/updateStudent")
-  public String updateStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
-    if (result.hasErrors()) {
-      return "updateStudent";
-    }
+  public ResponseEntity<String> updateStudent(@RequestBody StudentDetail studentDetail) {
     service.updateStudent(studentDetail);
-    return "redirect:/studentList";
+    return ResponseEntity.ok("更新処理が成功しました。");
   }
 }
