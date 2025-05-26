8abbeda2e85fb979b773e15e1c772a1613ea4642
Ryoma Inagaki <ryoma_bingo0509@ezweb.ne.jp>
2025-05-26 22:33:07 +0900

    スタンダード第23回課題 クラスStudentに入力チェック追加しました。 クラスStudentControllerに例外を発生させるメソッドgetTestExceptionを追加しました。 クラスGlobalExceptionHandlerを作成し、 MethodArgumentNotValidExceptionをキャッチして、フィールド名とメッセージをMapに整形するようにしました。 getTestExceptionも対応するようにしました。

---
+++ src/main/java/standard/StudentManagement/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package standard.StudentManagement.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getFieldErrors().forEach(error -> {
+      String fieldName = error.getField();
+      String errorMessage = error.getDefaultMessage();
+      errors.put(fieldName, errorMessage);
+    });
+    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+  }
+
+  @org.springframework.web.bind.annotation.ExceptionHandler(TestException.class)
+  public ResponseEntity<String> handleTestException(TestException ex){
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+  }
+}
