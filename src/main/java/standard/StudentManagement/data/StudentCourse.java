1f5178f5ad8b2e297cd0550c45d9d72c14536c1b
Ryoma Inagaki <ryoma_bingo0509@ezweb.ne.jp>
2025-06-25 22:46:17 +0900

    スタンダード第28回課題 準備 StudentCourseクラスに入力チェック追加しました。
    StudentDetailクラスのstudentCourseListに@Validを追加しました。

--- src/main/java/standard/StudentManagement/data/StudentCourse.java
+++ src/main/java/standard/StudentManagement/data/StudentCourse.java
@@ -1,6 +1,7 @@
 package standard.StudentManagement.data;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,9 +12,16 @@ import lombok.Setter;
 public class StudentCourse {
 
   private String id;
+
   private String studentId;
+
+  @NotBlank(message = "コース名は必須です。")
   private String courseName;
+
+  @NotBlank(message = "開始日は必須です。")
   private LocalDateTime startAt;
+
+  @NotBlank(message = "終了日は必須です。")
   private LocalDateTime endAt;
 
 }
