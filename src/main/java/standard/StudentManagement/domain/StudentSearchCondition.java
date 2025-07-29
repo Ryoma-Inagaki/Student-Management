a51bd5719e7e494dc2117047c70e0d96db4149e2
Ryoma Inagaki <ryoma_bingo0509@ezweb.ne.jp>
2025-07-29 23:36:25 +0900

    スタンダード第31回課題：条件検索機能の追加 refactor: 空欄での検索を許容するため、StudentSearchConditionの@Patternバリデーションを削除

--- src/main/java/standard/StudentManagement/domain/StudentSearchCondition.java
+++ src/main/java/standard/StudentManagement/domain/StudentSearchCondition.java
@@ -3,7 +3,6 @@ package standard.StudentManagement.domain;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -31,12 +30,10 @@ public class StudentSearchCondition {
   @Max(150)
   private int maxAge;
 
-  @Pattern(regexp = "男性|女性|その他")
   private String sex;
 
   @Size(max = 100)
   private String courseName;
 
-  @Pattern(regexp = "仮申込|本申込|受講中|受講終了")
   private String status;
 }
